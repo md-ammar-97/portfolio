dfcb167dfaf170b5830867b209fbf89162e87066
Mohammad Ammar <146890309+md-ammar-97@users.noreply.github.com>
2025-05-07 05:17:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -22,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio"> {/* Add the base path for GitHub Pages */}
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
